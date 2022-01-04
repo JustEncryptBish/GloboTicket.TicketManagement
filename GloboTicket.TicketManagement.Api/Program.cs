0b201289cf0382d896fc320e689c065d764279e8
Justin Carpenter <jcarpenter@arcwallsys.com>
2022-01-04 10:08:10 -0500

    Added Authenticatin/Authorization

--- GloboTicket.TicketManagement.Api/Program.cs
+++ GloboTicket.TicketManagement.Api/Program.cs
@@ -3,6 +3,11 @@ using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
 using Serilog;
 using Microsoft.OpenApi.Models;
+using GloboTicket.TicketManagement.Identity;
+using GloboTicket.TicketManagement.Api.Utility;
+using GloboTicket.TicketManagement.Api.Services;
+using Microsoft.AspNetCore.Identity;
+using GloboTicket.TicketManagement.Identity.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 AddSwagger(builder.Services);
@@ -12,24 +17,57 @@ Log.Logger = new LoggerConfiguration()
         .WriteTo.File("Logs/log-.txt", rollingInterval: RollingInterval.Day)
         .CreateLogger();
 
+
+
 void AddSwagger(IServiceCollection services)
 {
     services.AddSwaggerGen(c =>
     {
-        
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+            Name = "Authorization",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.ApiKey,
+            Scheme = "Bearer"
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+
         c.SwaggerDoc("v1", new OpenApiInfo
         {
             Version = "v1",
             Title = "GloboTicket Ticket Management API",
         });
 
-
+        c.OperationFilter<FileResultContentTypeOperationFilter>();
     });
 }
 
 builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddScoped<LoggedInUserService, LoggedInUserService>();
 builder.Services.AddControllers();
 
 builder.Services.AddCors(options =>
@@ -37,7 +75,24 @@ builder.Services.AddCors(options =>
     options.AddPolicy("Open", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 });
 
+
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+        GloboTicket.TicketManagement.Identity.Seed.UserCreator.SeedAsync(userManager).Wait();
+        Log.Information("Application User Seeded");
+    }
+    catch (Exception ex)
+    {
+        Log.Warning(ex, "An error occured while starting the application");
+    }
+}
 
 if (builder.Environment.IsDevelopment())
 {
@@ -47,6 +102,7 @@ if (builder.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -54,6 +110,8 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Management API");
 });
 app.UseCors("Open");
+
+app.UseAuthorization();
 
 app.MapControllers();
 
--- GloboTicket.TicketManagement.App/Program.cs
+++ GloboTicket.TicketManagement.App/Program.cs
@@ -1,32 +1,45 @@
+using AutoMapper;
 using Blazored.LocalStorage;
-using GloboTicket.TicketManagement.App;
 using GloboTicket.TicketManagement.App.Auth;
 using GloboTicket.TicketManagement.App.Contracts;
 using GloboTicket.TicketManagement.App.Services;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
 using System.Reflection;
+using System.Threading.Tasks;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("app");
-
-
-builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
-
-builder.Services.AddSingleton(new HttpClient
+namespace GloboTicket.TicketManagement.App
 {
-    BaseAddress = new Uri("https://localhost:5001")
-});
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:5001"));
+            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            builder.Services.AddBlazoredLocalStorage();
 
-builder.Services.AddScoped<IEventDataService, EventDataService>();
-builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
-builder.Services.AddScoped<IOrderDataService, OrderDataService>();
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 
-await builder.Build().RunAsync();
+
+            builder.Services.AddSingleton(new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:5001")
+            });
+
+            builder.Services.AddHttpClient<IClient, Client>(client => client.BaseAddress = new Uri("https://localhost:5001"));
+
+            builder.Services.AddScoped<IEventDataService, EventDataService>();
+            builder.Services.AddScoped<ICategoryDataService, CategoryDataService>();
+            builder.Services.AddScoped<IOrderDataService, OrderDataService>();
+            builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
